99b662a81b9458704b67843506b9cf366d87eec9
Anuar Jimenez <heyitsanuar@gmail.com>
2018-08-09 23:27:33 -0500

    Completed reveal on scroll behavior.

--- app/assets/js/App.js
+++ app/assets/js/App.js
@@ -1,3 +1,7 @@
 import Header from './modules/Header';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 var header = new Header();
+
+new RevealOnScroll($('.image'), "100%");
